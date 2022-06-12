cd3aa3f2b80dfed123eff8eceff15a4e5d5b3d05
swg-wl <zavgorodniy.v.v@gmail.com>
2022-06-12 17:10:34 +0300

    Add blockly component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlocklyComponent } from './blockly/blockly.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlocklyComponent
   ],
   imports: [
     BrowserModule
